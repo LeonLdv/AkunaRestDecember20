9ca00b53aa99c7786d1a51906547600b4d27a34b
Maksym Leonenko <max23.sumy.ua@gmail.com>
2022-03-17 17:12:41 +0200

    Updated.

--- Plugins/Nop.Plugin.Widgets.NivoSlider/NivoSliderPlugin.cs
+++ Plugins/Nop.Plugin.Widgets.NivoSlider/NivoSliderPlugin.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Nop.Core;
 using Nop.Core.Infrastructure;
--- Plugins/Nop.Plugin.Widgets.NovaPoshta/ShippingWidgets.cs
+++ Plugins/Nop.Plugin.Widgets.NovaPoshta/ShippingWidgets.cs
@@ -1,31 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Nop.Core;
 using Nop.Plugin;
 using Nop.Services.Cms;
+using Nop.Services.Configuration;
+using Nop.Services.Localization;
 using Nop.Services.Plugins;
 using Nop.Web.Framework.Infrastructure;
 
 namespace Nop.Plugin.Widgets.NovaPoshta
 {
-    public class ShippingWidgets : BasePlugin, IWidgetPlugin
+    public class ShippingWidgets : BasePlugin
     {
-        public bool HideInWidgetList => true;
 
-        string IWidgetPlugin.GetWidgetViewComponentName(string widgetZone)
+        //private readonly ILocalizationService _localizationService;
+        //private readonly ISettingService _settingService;
+        //private readonly IWebHelper _webHelper;
+
+        //public ShippingWidgets(ILocalizationService localizationService,
+        // ISettingService settingService,
+        // IWebHelper webHelper)
+        //{
+        //    _localizationService = localizationService;
+        //    _settingService = settingService;
+        //    _webHelper = webHelper;
+        //}
+        //public bool HideInWidgetList => true;
+        //public Task<IList<string>> GetWidgetZonesAsync()
+        //{
+        //    return Task.FromResult<IList<string>>(new List<string> { PublicWidgetZones.CheckoutShippingMethodTop });
+        //}
+
+        //public string GetWidgetViewComponentName(string widgetZone)
+        //{
+        //    return "WidgetNovaPoshta";
+        //}
+
+
+        //public override string GetConfigurationPageUrl()
+        //{
+        //    return _webHelper.GetStoreLocation() + "Admin/WidgetsNovaPoshta/Configure";
+        //}
+
+        public override async Task InstallAsync()
         {
-            throw new NotImplementedException();
+            await base.InstallAsync();
         }
 
-        public Task<IList<string>> GetWidgetZonesAsync()
+        public override async Task UninstallAsync()
         {
-            return Task.FromResult<IList<string>>(new List<string> { PublicWidgetZones.CheckoutShippingMethodTop });
+            await base.UninstallAsync();
         }
 
-        public string GetWidgetViewComponentName(string widgetZone)
-        {
-            return "WidgetNovaPoshta";
-        }
+
     }
 }
 
